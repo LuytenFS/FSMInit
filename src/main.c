63d90d432f8a34e8128a987fab266dadbfb8f055
LuytenKy <luytenky@outlook.com>
2026-04-10 15:45:34 +0300

    Windows jargon

--- src/main.c
+++ src/main.c
@@ -171,7 +171,8 @@ int main(int argc, char *argv[])
     printf("\n%s%s--- Summary ---%s\n", TEX_BOLD, COL_CYAN, COL_RESET);
     printf("  %sDirectories:%s %s%d%s\n", TEX_BOLD, COL_RESET, COL_GREEN, operation.dirs_created, COL_RESET);
     printf("  %sTables:%s     %s%d%s\n", TEX_BOLD, COL_RESET, COL_GREEN, operation.tables_created, COL_RESET);
-    printf("  %sErrors:%s     %s%d%s\n", TEX_BOLD, COL_RESET, operation.errors > 0 ? COL_RED : COL_GREEN, operation.errors, COL_RESET);
+    printf("  %sErrors:%s     %s%d%s\n", TEX_BOLD, COL_RESET,
+           (operation.errors > 0 ? COL_RED : COL_GREEN), operation.errors, COL_RESET);
     printf("%s%s----------------%s\n", TEX_BOLD, COL_CYAN, COL_RESET);
 
     if (operation.debug)
